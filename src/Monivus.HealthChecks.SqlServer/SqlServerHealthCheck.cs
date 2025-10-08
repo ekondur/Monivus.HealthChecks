295ddc40f4bb61cccdd2b63b23acf9386e99752d
ekondur <emrahkondur@hotmail.com>
2025-10-08 14:43:37 +0300

    Upgraded sqlclient package

--- src/Monivus.HealthChecks.SqlServer/SqlServerHealthCheck.cs
+++ src/Monivus.HealthChecks.SqlServer/SqlServerHealthCheck.cs
@@ -1,4 +1,4 @@
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -89,3 +89,4 @@ namespace Monivus.HealthChecks.SqlServer
         }
     }
 }
+
